b4a6b2e06ce4d9cded4bede0cd8b1a4b68fafeec
Ronel Cruz <ronel.cruz.a8@gmail.com>
2023-01-30 13:45:57 -0400

    installed adding initial phase of the api plugin

---
+++ src/Plugins/Nop.Plugin.Misc.FluidApi/FluidApiDefaults.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Nop.Plugin.Misc.FluidApi
+{
+	public class FluidApiDefaults
+	{
+        /// <summary>
+        /// Gets a plugin system name
+        /// </summary>
+        public static string SystemName => "Misc.FluidApi";
+    }
+}
+
---
+++ src/Plugins/Nop.Plugin.Misc.FluidApi/FluidApiSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Misc.FluidApi
+{
+	public class FluidApiSettings: ISettings
+    {
+        public bool enabled { get; set; }
+    }
+}
+
